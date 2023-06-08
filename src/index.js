7e6f62781335a518edc6bcca24a5d648047fae60
khushboo <khushboo.panwar@hauper.com>
2023-06-08 16:39:03 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { MovieContextProvider } from "./context/MovieContext";
-import { BrowserRouter as Router } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import Navbar from "./Components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <MovieContextProvider>
         <Navbar />
         <App />
       </MovieContextProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
